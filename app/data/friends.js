128f6549a18ffde3b4fb7ce4d4d15d4c9f6e9878
Lane Anderson <laneanderson92@gmail.com>
2017-11-19 23:07:42 -0600

    Added Routes and Get/Post logic

---
+++ app/data/friends.js
@@ -0,0 +1,11 @@
+// Below is where the data for added friends will be stored.
+
+var friendArray = [
+	{
+		name: 'Lane',
+		image: 'google.com/images',
+		scores: [4,3,2,5,4,3,2,1,2,4,5]
+	}
+]
+
+module.exports = friendArray;
